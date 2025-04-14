a08ff98bf27d233c00dabb6b10d0d99193b50a7d
Fidan <xelilovafidan61@gmail.com>
2025-04-14 23:19:55 +0400

    card

--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -1,8 +1,9 @@
-import './Home.css'
+
 import Navbar from '../../components/Home/Navbar/Navbar'
 import Header from '../../components/Home/Header/Header'
-import Projects from '../../components/Home/Card/Project/HomeCardProjects'
-import Mentorium from '../../components/Home/Card/Mentorium/HomeCardMentorium'
+import Card from '../../components/Home/Card/index'
+import photo from '../../assets/images/risImage.png'
+import background from '../../assets/images/card-image.png'
 
 
 export default function Home() {
@@ -12,12 +13,34 @@ export default function Home() {
             <Header />
             <h1>Layihələr</h1>
             <div className="cards">
-                <Projects />
-                <Projects />
-                <Projects />
-                <Mentorium />
-                <Mentorium />
-                <Mentorium />
+                <Card 
+                type = 'project'
+                bgImg = {background}
+                title = 'Restoran idarəetmə sistemi'
+                topicDescription = 'Restoranların masa rezervasiyası, sifarişlərin idarəsi və stok nəzarətini avtomatlaşdıran Java əsaslı masaüstü tətbiqdir.'
+                profilePhoto = {photo}
+                name = 'Murad Fərəczadə'
+                job = 'UX / UI Designer'
+                date = '01.04.2025'
+                participantCount = {12}
+                totalParticipants = {25}
+                percentage = {20}
+                />
+
+                <Card 
+                type = 'mentorium'
+                bgImg = {background}
+                title = 'Restoran idarəetmə sistemi'
+                topicDescription = 'Restoranların masa rezervasiyası, sifarişlərin idarəsi və stok nəzarətini avtomatlaşdıran Java əsaslı masaüstü tətbiqdir.'
+                profilePhoto = {photo}
+                name = 'Murad Fərəczadə'
+                job = 'UX / UI Designer'
+                date = '01.04.2025'
+                participantCount = {12}
+                totalParticipants = {25}
+                percentage = {20}
+                />
+                
             </div>
         </div>
 
