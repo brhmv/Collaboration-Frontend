9bd7164d2f4f1c643801f41eb59bc40765ee42a7
ferhad <137129786+ferhadnaiyev@users.noreply.github.com>
2025-04-15 12:01:13 +0400

    map example

--- src/pages/ApplicationsPage/SavedPage/page.jsx
+++ src/pages/ApplicationsPage/SavedPage/page.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
-
+import Card from '@/components/Home/Card/index'
+import photo from '@/assets/images/risImage.png'
+import background from '@/assets/images/card-image.png'
+import { SavedCardsContent } from './data/mock__data'
 function SavedPage() {
     return (
-        <div>
+        <div className='grid grid-cols-3 gap-[32px] mt-[70px]'>
+            {
+                SavedCardsContent && SavedCardsContent.map((item) => (
+                    <Card
+                        key={item.id}
+
+                        bgImg={background} //temporary
+                        profilePhoto={photo} //temporary
+                        type={item.type}
+                        title={item.title}
+                        topicDescription={item.topicDescription}
+                        name={item.name}
+                        job={item.job}
+                        date={item.date}
+                        participantCount={item.participantCount}
+                        totalParticipants={item.totalParticipants}
+                        percentage={item.percentage}
+                    // or
+                    // {...item}
+                    />
+                ))
+            }
 
         </div>
     )
